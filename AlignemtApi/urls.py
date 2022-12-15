418d41d1fbcd83febb029e078cbcf18668eb1228
mohmmed fathi <mohmmedfathi.123@gmail.com>
2022-12-15 22:40:01 +0200

    remove swagger

--- AlignemtApi/urls.py
+++ AlignemtApi/urls.py
@@ -15,25 +15,25 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import url
+
 from Global.views import Global_Get_Post
 from rest_framework import permissions
 from . import swagger
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
+# from drf_yasg.views import get_schema_view
+# from drf_yasg import openapi
 
-schema_view = get_schema_view(
-    openapi.Info(
-        title="INCOME EXPENSES API",
-        default_version='v1',
-        description="Test description",
-        terms_of_service="https://www.ourapp.com/policies/terms/",
-        contact=openapi.Contact(email="contact@expenses.local"),
-        license=openapi.License(name="Test License"),
-    ),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
-)
+# schema_view = get_schema_view(
+#     openapi.Info(
+#         title="INCOME EXPENSES API",
+#         default_version='v1',
+#         description="Test description",
+#         terms_of_service="https://www.ourapp.com/policies/terms/",
+#         contact=openapi.Contact(email="contact@expenses.local"),
+#         license=openapi.License(name="Test License"),
+#     ),
+#     public=True,
+#     permission_classes=(permissions.AllowAny,),
+# )
 
 
 urlpatterns = [
